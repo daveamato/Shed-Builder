caaca5ec695b2dcc86d6e645ea2e9f4c26151f08
Taylor Jackson <tjack94@hotmail.com>
2019-01-28 09:00:16 -0700

    created shed types service

---
+++ src/shedTyes.js
@@ -0,0 +1,38 @@
+export const ShedTypes = [
+	{
+		name: 'Value Apex',
+		image: 'https://adobe.ly/2UhCsHw',
+		description:
+			'Our Value Apex or Ranch Style Storage Shed comes with 6 ft. side walls and a 4" overhang on the sides. These are extremely affordable and still give plenty of room for whatever you need to store.'
+	},
+	{
+		name: 'Standard Apex',
+		image: 'https://adobe.ly/2UhBP0C',
+		description:
+			'Our Standard Apex or Ranch Style Storage Shed comes with 7 ft. side walls and a 4" overhang on the sides. These are our most popular storage buildings and can be customized any many different ways.'
+	},
+	{
+		name: 'Tall Apex',
+		image: 'https://adobe.ly/2UlqcFO',
+		description:
+			'Our Tall Apex or Ranch Style Storage Shed comes with 8 ft. side walls and a 4" overhang on the sides. These tall enough for all your big storage needs can be customized any many different ways.'
+	},
+	{
+		name: 'Lean-to',
+		image: 'https://adobe.ly/2UlND1S',
+		description:
+			'Our Lean-to Sheds are great placed in areas where space is limited. They can be placed up against another structure and the doors can be placed on any of the side walls.'
+	},
+	{
+		name: 'Tall Barn',
+		image: 'https://adobe.ly/2B7UlRP',
+		description:
+			"Our Tall Barn style storage sheds come with 7' ft. tall side walls and a barn style gambrel roof. These sheds allow more room for a loft for that extra storage space you need."
+	},
+	{
+		name: 'Tack Shed',
+		image: 'https://adobe.ly/2SbK7JY',
+		description:
+			'Our Tack-Style Sheds are great for those who are looking for a more rustic look or a shed with a porch to lounge on. These sheds can be customized in many different ways.'
+	}
+];
